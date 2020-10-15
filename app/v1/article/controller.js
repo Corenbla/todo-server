1f0320f4c4c77685504361b023a9bc6c36d715cb
Corentin <corenbla@gmail.com>
2020-10-15 10:42:32 +0200

    article controller refactor

--- app/v1/article/controller.js
+++ app/v1/article/controller.js
@@ -8,8 +8,8 @@ const {createSchema} = require('./schema.js');
 const getAll = async (req, res) => {
     const {title} = req.query;
 
-    const [listErr, list] = await to(
-        db('vw_list')
+    const [articleErr, article] = await to(
+        db('article')
             .select()
             .where((builder) =>
                 !R.isNil(title)
@@ -17,39 +17,39 @@ const getAll = async (req, res) => {
                     : builder,
             ),
     );
-    if (!R.isNil(listErr)) {
-        logger.error(listErr);
-        return res.status(500).json({error: `${listErr}`});
+    if (!R.isNil(articleErr)) {
+        logger.error(articleErr);
+        return res.status(500).json({error: `${articleErr}`});
     }
 
-    return res.status(200).json(list);
+    return res.status(200).json(article);
 };
 
 const getById = async (req, res) => {
     const {id} = req.params;
 
-    const [listErr, list] = await to(db('vw_list').select().where({id}));
-    if (!R.isNil(listErr)) {
-        logger.error(`${listErr}`);
-        return res.status(500).json({error: `${listErr}`});
+    const [articleErr, article] = await to(db('article').select().where({id}));
+    if (!R.isNil(articleErr)) {
+        logger.error(`${articleErr}`);
+        return res.status(500).json({error: `${articleErr}`});
     }
 
-    const [itemsErr, items] = await to(db('item').select().where({list_id: id}));
-    if (!R.isNil(itemsErr)) {
-        logger.error(`${itemsErr}`);
-        return res.status(500).json({error: `${itemsErr}`});
+    const [commentErr, comments] = await to(db('comment').select().where({article_id: id}));
+    if (!R.isNil(commentErr)) {
+        logger.error(`${commentErr}`);
+        return res.status(500).json({error: `${commentErr}`});
     }
 
-    if (R.isEmpty(list)) {
-        const error = `No list for id ${id}`;
+    if (R.isEmpty(article)) {
+        const error = `No article for id ${id}`;
         logger.error(error);
         return res.status(400).json({error});
     }
 
-    return res.status(200).json({...list[0], items});
+    return res.status(200).json({...article[0], comments: comments});
 };
 
-const createList = async (req, res) => {
+const createArticle = async (req, res) => {
     // Validate input with Joi schema
     const {error: schemaErr, value: body} = createSchema.validate(req.body);
     if (!R.isNil(schemaErr)) {
@@ -58,22 +58,22 @@ const createList = async (req, res) => {
         return res.status(400).json({error});
     }
 
-    const [listErr, list] = await to(db('list').insert(body).returning('*'));
-    if (!R.isNil(listErr)) {
-        logger.error(`${listErr}`);
-        return res.status(500).json({error: `${listErr}`});
+    const [articleErr, article] = await to(db('article').insert(body).returning('*'));
+    if (!R.isNil(articleErr)) {
+        logger.error(`${articleErr}`);
+        return res.status(500).json({error: `${articleErr}`});
     }
 
-    if (R.isEmpty(list)) {
+    if (R.isEmpty(article)) {
         const error = 'No row written';
         logger.error(error);
         return res.status(500).json({error});
     }
 
-    return res.status(200).json(list[0]);
+    return res.status(200).json(article[0]);
 };
 
-const updateList = async (req, res) => {
+const updateArticle = async (req, res) => {
     const {id} = req.params;
 
     // Validate input with Joi schema
@@ -84,44 +84,44 @@ const updateList = async (req, res) => {
         return res.status(400).json({error});
     }
 
-    const [listErr, list] = await to(
-        db('list')
+    const [articleErr, article] = await to(
+        db('article')
             .update({...body, updated_at: new Date()})
             .where({id})
             .returning('*'),
     );
-    if (!R.isNil(listErr)) {
-        logger.error(`${listErr}`);
-        return res.status(500).json({error: `${listErr}`});
+    if (!R.isNil(articleErr)) {
+        logger.error(`${articleErr}`);
+        return res.status(500).json({error: `${articleErr}`});
     }
 
-    if (R.isEmpty(list)) {
-        const error = `No list for id ${id}`;
+    if (R.isEmpty(article)) {
+        const error = `No article for id ${id}`;
         logger.error(error);
         return res.status(500).json({error});
     }
 
-    return res.status(200).json(list[0]);
+    return res.status(200).json(article[0]);
 };
 
-const deleteList = async (req, res) => {
+const deleteArticle = async (req, res) => {
     const {id} = req.params;
 
-    const [listErr, list] = await to(
-        db('list').del().where({id}).returning('*'),
+    const [articleErr, article] = await to(
+        db('article').del().where({id}).returning('*'),
     );
-    if (!R.isNil(listErr)) {
-        logger.error(`${listErr}`);
-        return res.status(500).json({error: `${listErr}`});
+    if (!R.isNil(articleErr)) {
+        logger.error(`${articleErr}`);
+        return res.status(500).json({error: `${articleErr}`});
     }
 
-    if (R.isEmpty(list)) {
-        const error = `No list for id ${id}`;
+    if (R.isEmpty(article)) {
+        const error = `No article for id ${id}`;
         logger.error(error);
         return res.status(500).json({error});
     }
 
-    return res.status(200).json(list[0]);
+    return res.status(200).json(article[0]);
 };
 
-module.exports = {getAll, getById, createArticle: createList, updateArticle: updateList, deleteArticle: deleteList};
+module.exports = {getAll, getById, createArticle: createArticle, updateArticle: updateArticle, deleteArticle: deleteArticle};
